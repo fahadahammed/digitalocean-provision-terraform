af07990de9ddab704440c3912dc12f4e154b9e39
fahadahammed <iamfahadahammed@gmail.com>
2022-07-06 08:47:43 +0600

    Terraform output reference fix

--- terraform-digitalocean/output.tf
+++ terraform-digitalocean/output.tf
@@ -1,19 +1,19 @@
 output "droplet-private-ip" {
-  value = digitalocean_droplet.static-cdn-origin.ipv4_address_private
+  value = digitalocean_droplet.my_desired_do_droplet.ipv4_address_private
 }
 
 output "droplet-public-ip" {
-  value = digitalocean_droplet.static-cdn-origin.ipv4_address
+  value = digitalocean_droplet.my_desired_do_droplet.ipv4_address
 }
 
 output "droplet-tags" {
-  value = digitalocean_droplet.static-cdn-origin.tags
+  value = digitalocean_droplet.my_desired_do_droplet.tags
 }
 
 output "droplet-name" {
-  value = digitalocean_droplet.static-cdn-origin.name
+  value = digitalocean_droplet.my_desired_do_droplet.name
 }
 
 output "droplet-hourly-price" {
-  value = digitalocean_droplet.static-cdn-origin.price_hourly
+  value = digitalocean_droplet.my_desired_do_droplet.price_hourly
 }
